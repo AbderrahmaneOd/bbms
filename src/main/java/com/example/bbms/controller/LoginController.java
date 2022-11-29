876ecd1af9d99bba222294c737a4401e67fc9fc4
Abderrahmane Ouaday <abdoouday361@gmail.com>
2022-11-29 02:58:28 +0100

    Update design and login controller

--- src/main/java/com/example/bbms/controller/LoginController.java
+++ src/main/java/com/example/bbms/controller/LoginController.java
@@ -15,6 +15,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -27,45 +28,97 @@ public class LoginController implements Initializable {
     private PasswordField password;
 
     @FXML
-    private TextField userName;
+    private TextField userId;
     @FXML
     private ChoiceBox<String> userType;
     final private String[] userTypes = {"Admin", "Hospital", "Blood Bank"};
 
-    @FXML
-    void signIn(ActionEvent event) throws IOException {
+    Connection con;
+    PreparedStatement pst;
 
-        if(userName.getText().equals("a") && password.getText().equals("a")){
-            root = FXMLLoader.load(MainApplication.class.getResource("view/Admin-view.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setTitle("Admin Dashboard");
-            stage.setScene(scene);
-            stage.show();
-        }else if(userName.getText().equals("h") && password.getText().equals("h")){
-            root = FXMLLoader.load(MainApplication.class.getResource("view/Hospital-view.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setTitle("Hospital Dashboard");
-            stage.setScene(scene);
-            stage.show();
+    // Connect to DB
+    public void Connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        }else if(userName.getText().equals("b") && password.getText().equals("b")){
-            root = FXMLLoader.load(MainApplication.class.getResource("view/BloodBank-view.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setTitle("Blood Bank Dashboard");
-            stage.setScene(scene);
-            stage.show();
+            con = DriverManager.getConnection("jdbc:mysql://localhost/bbms", "root", "abderrahmane");
+        } catch (ClassNotFoundException ex) {
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-
-
     }
 
+    @FXML
+    void signIn(ActionEvent event) throws IOException {
+        int userNameVariable = Integer.parseInt(userId.getText());
+        String userPasswordVariable = "";
+
+        if (userType.getValue().equals("Hospital")) {
+            try {
+                pst = con.prepareStatement("SELECT password_h FROM hospital WHERE id_h = ?");
+                pst.setInt(1, userNameVariable);
+                // Admin
+                ResultSet resultSet = pst.executeQuery();
+                if (resultSet.next())
+                    userPasswordVariable = resultSet.getString("password_h");
+
+                if (userPasswordVariable.equals(password.getText())) {
+                    root = FXMLLoader.load(MainApplication.class.getResource("view/Hospital-view.fxml"));
+                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setTitle("Hospital Dashboard");
+                    stage.setScene(scene);
+                    stage.show();
+                }
+
+            } catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+            }
+        } else if (userType.getValue().equals("Blood Bank")) {
+            try {
+                pst = con.prepareStatement("SELECT password_bk FROM blood_bank WHERE id_bk = ?");
+                pst.setInt(1, userNameVariable);
+                // Admin
+                ResultSet resultSet = pst.executeQuery();
+                if (resultSet.next())
+                    userPasswordVariable = resultSet.getString("password_bk");
+
+                if (userPasswordVariable.equals(password.getText())) {
+                    root = FXMLLoader.load(MainApplication.class.getResource("view/BloodBank-view.fxml"));
+                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setTitle("Blood Bank Dashboard");
+                    stage.setScene(scene);
+                    stage.show();
+                }
+
+            } catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+            }
+
+        }else if(userType.getValue().equals("Admin")){
+            if(userId.getText().equals("1") && password.getText().equals("a")){
+                root = FXMLLoader.load(MainApplication.class.getResource("view/Admin-view.fxml"));
+                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setTitle("Admin Dashboard");
+                stage.setScene(scene);
+                stage.show();
+            }
+
+        }
+
+        }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userType.getItems().addAll(userTypes);
         userType.setValue("User Type");
+        Connect();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.bbms {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.java;
 
 
     opens com.example.bbms to javafx.fxml;
